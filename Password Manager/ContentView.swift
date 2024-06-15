aea4bf600324620aa46d9c46881607e3a754e78a
Mohd Kashif <kashifmd123@gmail.com>
2024-06-15 11:06:01 +0530

    Initial Commit

---
+++ Password Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Password Manager
+//
+//  Created by Mohd Kashif on 15/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Password Manager/Password_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_ManagerApp.swift
+//  Password Manager
+//
+//  Created by Mohd Kashif on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Password_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
